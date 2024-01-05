665c400fe9a2f8be79c036e95b1835dab5d3cdbf
Nicolas Zabik <melchiah.nico@gmail.com>
2024-01-05 18:36:53 +0100

    ajout du panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Item;
+use App\Repository\ItemRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/cart', name: 'cart_')]
+class CartController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(SessionInterface $session, ItemRepository $itemRepository): Response
+    {
+        $panier = $session->get('panier', []);
+        $data = [];
+        $total = 0;
+
+        foreach ($panier as $id => $quantity) {
+            $product = $itemRepository->find($id);
+            $data[] = [
+                'product' => $product,
+                'quantity' => $quantity
+            ];
+            $total += $product->getPrice() * $quantity;
+        }
+
+        return $this->render('cart/index.html.twig', compact('data', 'total'));
+    }
+    #[Route('/add/{id}', name: 'add')]
+    public function add(Item $item, SessionInterface $session)
+    {
+        // on récupère l'ID produit
+        $id = $item->getId();
+        // on récupère le panier existant
+        $panier = $session->get('panier', []);
+        // on ajoute le produit dans le pannier s'il n'y est pas, ou sinon, on incrémente sa quantité
+        if (empty($panier[$id])) {
+            $panier[$id] = 1;
+        } else {
+            $panier[$id]++;
+        }
+        $session->set('panier', $panier);
+        // on redirige vers le panier
+        return $this->redirectToRoute('cart_index');
+    }
+    #[Route('/remove/{id}', name: 'remove')]
+    public function remove(Item $item, SessionInterface $session)
+    {
+        // on récupère l'ID produit
+        $id = $item->getId();
+        // on récupère le panier existant
+        $panier = $session->get('panier', []);
+        // on retire le produit dans le pannier s'il n'y a qu'un exemplaire, sinon on décrémente la quantité
+        if (!empty($panier[$id])) {
+            if ($panier[$id] > 1) {
+                $panier[$id]--;
+            
+        } else {
+            unset($panier[$id]);
+        }
+    }
+        $session->set('panier', $panier);
+        // on redirige vers le panier
+        return $this->redirectToRoute('cart_index');
+    }
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(Item $item, SessionInterface $session)
+    {
+        // on récupère l'ID produit
+        $id = $item->getId();
+        // on récupère le panier existant
+        $panier = $session->get('panier', []);
+        // on retire le produit dans le pannier
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+        $session->set('panier', $panier);
+        // on redirige vers le panier
+        return $this->redirectToRoute('cart_index');
+    }
+    #[Route('/empty', name: 'empty')]
+    public function empty(SessionInterface $session)
+    {
+        $session->remove('panier');
+        // on redirige vers le panier
+        return $this->redirectToRoute('cart_index');
+    }
+}
