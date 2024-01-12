e186dff1cee8adc76ac486fb7aae267bb43286c1
Nicolas Zabik <melchiah.nico@gmail.com>
2024-01-12 16:17:05 +0100

    page home intéractive en fonction des produits et des différentes catégories présentes dans la BDD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ItemRepository;
 use App\Repository\LogoRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,13 +16,17 @@ class HomeController extends AbstractController
     // {
     //     return $this->render('pages/home.html.twig');
     // }
-    public function index(ItemRepository $itemRepository): Response
+    public function index(ItemRepository $itemRepository, CategoryRepository $categoryRepository): Response
     {
+        $item = $itemRepository->findAll();
+        $category = $categoryRepository->findAll();
         $mobiles = $itemRepository->findBy(['category' => 1]);
         $watches = $itemRepository->findBy(['category' => 2]);
         return $this->render('pages/home.html.twig', [
             'mobiles' => $mobiles,
-            'watches' => $watches
+            'watches' => $watches,
+            'items' => $item,
+            'categories' => $category
         ]);
     }
     #[Route('/logo/{id}', name: 'render_logo')]
